9674d8d070c06382e124cd96e1bd370c1f1f7d4a
Sandara  Hettiarachchi <apoorwasandara@gmail.com>
2025-04-17 11:25:33 +0530

    Feat : updated Main Class accordingly

--- src/main/java/bookstore/Main.java
+++ src/main/java/bookstore/Main.java
@@ -1,7 +1,26 @@
 package bookstore;
 
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static final String BASE_URI = "http://localhost:8080/api/";
+
+    public static HttpServer startServer() {
+        final ResourceConfig config = new ResourceConfig()
+                .packages("bookstore.resources"); // Your resource package
+        return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), config);
+    }
+
+    public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println("Bookstore API started at " + BASE_URI);
+        System.out.println("Press enter to stop...");
+        System.in.read();
+        server.shutdownNow();
     }
 }
